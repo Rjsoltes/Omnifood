7226e2a52113d13b4faacd36ce75b56351063303
Ryan Soltes <rjsoltes@gmail.com>
2017-01-18 16:29:31 -0500

    First Commit

---
+++ resources/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    
+     /*Scroll on buttons*/
+    $(".js-scroll-to-plan").click(function () {
+        $("html, body").animate({scrollTop: $("js-section-plan").offset().top}, 1000);
+    });
+    
+    
+    
+    
+});
+
+
+
+   
